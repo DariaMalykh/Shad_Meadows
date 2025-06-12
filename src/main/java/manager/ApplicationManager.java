5392defc0c13e13af7175dd58cf1034ea72a3d64
DariaMalykh <159196750+DariaMalykh@users.noreply.github.com>
2025-06-12 18:40:52 +0300

    New Project

---
+++ src/main/java/manager/ApplicationManager.java
@@ -0,0 +1,16 @@
+package manager;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class ApplicationManager {
+    WebDriver wd;
+
+    public void init(){
+        wd = new ChromeDriver();
+        wd.navigate().to("https://automationintesting.online/");
+    }
+    public void stop(){
+        wd.quit();
+    }
+}
---
+++ src/test/java/tests/LoginTests.java
@@ -0,0 +1,15 @@
+package tests;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class LoginTests extends TestBase {
+
+    @Test
+    public void test(){
+
+    }
+
+
+}
